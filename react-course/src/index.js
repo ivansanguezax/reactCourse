64c48e9fd651337c4f208ecc5d22d9d87a8d82fc
ivansanguezax <ivansanguezax@gmail.com>
2023-09-26 02:02:11 -0400

    componentes externos

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Greeting, UserCard } from "./Greeting";
-import Product, { Navbar } from "./Product";
-import {MyButton} from "./Button";
-import { TaskCard } from "./Tarea";
-import { Saludar } from "./Saludar";
 import {Post} from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
