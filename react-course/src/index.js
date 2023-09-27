d6af71aadf5c0b84140a5e724f1c534e318131a0
ivansanguezax <ivansanguezax@gmail.com>
2023-09-26 23:00:24 -0400

    useState

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import { Post } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const user = [
-  {
-    id: 1,
-    name: "John",
-    image: "https://robohash.org/user2",
-  },
-  {
-    id: 2,
-    name: "Pedro",
-    image: "https://robohash.org/user1",
-  },
-];
 
-root.render(
-  <>
-    {user.map((user, indice) => {
-      return (
-        <div key={indice}>
-          <h1>{user.name}</h1>
-          <img src={user.image} alt={user.name} />
-        </div>
-      );
-    })}
-  </>
-);
+
+function Counter() {
+  let [ mensaje, setMensaje ] = useState('');
+
+  return (
+    <>
+      <input 
+        onChange={(e) => setMensaje(e.target.value)}
+      />
+      <button 
+        onClick={() => {
+          alert('el mensaje es ' +mensaje);
+        }}
+      >Save</button>
+
+    </>
+  );
+}
+
+root.render(<>
+  <Counter />
+</>);
