8aed82ffa5bb5a010de9257ffe9dec6a2701c058
ivansanguezax <ivansanguezax@gmail.com>
2023-09-26 23:08:52 -0400

    useEffect

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,29 +1,41 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Post } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
 function Counter() {
-  let [ mensaje, setMensaje ] = useState('');
+  let [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("se ejecuto el useEffect");
+  }, [counter]);
 
   return (
     <>
-      <input 
-        onChange={(e) => setMensaje(e.target.value)}
-      />
-      <button 
+      <input onChange={(e) => setMensaje(e.target.value)} />
+      <button
         onClick={() => {
-          alert('el mensaje es ' +mensaje);
+          alert("el mensaje es " + mensaje);
         }}
-      >Save</button>
+      >
+        Save
+      </button>
+
+      <hr/>
+      <h1>Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}>Incrementar</button>
 
     </>
   );
 }
 
-root.render(<>
-  <Counter />
-</>);
+root.render(
+  <>
+    <Counter />
+  </>
+);
