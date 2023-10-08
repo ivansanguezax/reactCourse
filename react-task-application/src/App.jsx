4c4a342bd1a32c1255a8ee527fdfa4a87601c2c9
ivansanguezax <ivansanguezax@gmail.com>
2023-10-08 00:40:27 -0400

    añadir tareas

--- react-task-application/src/App.jsx
+++ react-task-application/src/App.jsx
@@ -1,11 +1,28 @@
-import TaskList from './TaskList'
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import {task as data} from './task'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [tasks, setTask] = useState([]);
+  useEffect(() => {
+    setTask(data);
+  }, []);
+
+  function createTask(taskTitle) {
+    setTask([...tasks,{
+      id: tasks.length + 1,
+      title: taskTitle,
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam."
+    }]);
+  }
+
   return (
-    <div>
-      <TaskList/>
-    </div>
-  )
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks}/>
+    </>
+  );
 }
 
-export default App
+export default App;
--- react-task-application/src/TaskList.jsx
+++ react-task-application/src/TaskList.jsx
@@ -1,33 +1,24 @@
-import {task as data} from './task'
-import {useState, useEffect} from 'react'
+import PropTypes from 'prop-types';
 
-
-
-function TaskList() {
+function TaskList({tasks}) {
   
-    const [task, setTask] = useState([]);
-    useEffect(() => {
-        setTask(data);
-        },[]);
-
-    if (task.length === 0) {
+    
+    if (tasks.length === 0) {
      return <h1>No hay tareas</h1>
     }
-
-    
-  
     return (
     <div>
-      {
-        task.map((task) => (
+      {tasks.map((task) => (
           <div key={task.id}>
             <h1>{task.title}</h1>
             <p>{task.description}</p>
           </div>
-        ))
-      }
+        ))}
     </div>
   )
 }
+TaskList.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
 
 export default TaskList
